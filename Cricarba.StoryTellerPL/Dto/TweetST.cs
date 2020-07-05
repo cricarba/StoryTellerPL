ae8e1b6a7b36f4fe81d6511986a78ba5369d20ce
cricarba <carvagereal@gmail.com>
2020-07-04 20:57:48 -0500

    secon time ends

--- Cricarba.StoryTellerPL/Dto/TweetST.cs
+++ Cricarba.StoryTellerPL/Dto/TweetST.cs
@@ -14,6 +14,6 @@ namespace Cricarba.StoryTellerPL.Dto
         public bool HasImage { get; set; }
         public string Image { get; set; }
         public bool  IsHalfTime { get { return Template.Contains("Half-time"); } }
-        public bool IsEndTime { get { return Template.Contains("Full-time")|| Template.Contains("Second Half"); } }
+        public bool IsEndTime { get { return Template.Contains("Full-time")|| Template.Contains("Second Half ends"); } }
     }
 }
