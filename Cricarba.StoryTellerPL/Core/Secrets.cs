821028aea65d50041b97141dfea45b83efcedad8
Cristian Carvajal Bahamon <cristian.carvajal@arandasoft.com>
2020-12-15 13:49:09 -0500

    SECRETOS LOCALES

--- Cricarba.StoryTellerPL/Core/Secrets.cs
+++ Cricarba.StoryTellerPL/Core/Secrets.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿// <copyright company="Aranda Software">
+// © Todos los derechos reservados
+// </copyright>
+
 using Azure.Core;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using System;
 
 namespace Cricarba.StoryTellerPL.Core
 {
@@ -9,21 +13,29 @@ namespace Cricarba.StoryTellerPL.Core
     {
         public string GetSecrects(string name)
         {
-            SecretClientOptions options = new SecretClientOptions()
+            bool.TryParse(Environment.GetEnvironmentVariable("SECRETS_LOCAL"), out bool local);
+            if (local)
             {
-                Retry =
+                return Environment.GetEnvironmentVariable(name);
+            }
+            else
+            {
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
                         {
                             Delay= TimeSpan.FromSeconds(2),
                             MaxDelay = TimeSpan.FromSeconds(16),
                             MaxRetries = 5,
                             Mode = RetryMode.Exponential
                          }
-            };
+                };
 
-            var kvUri = $"https://{Environment.GetEnvironmentVariable("KEY_VAULT_NAME")}.vault.azure.net";
-            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
-            KeyVaultSecret secret = client.GetSecret(name);
-            return secret.Value;
+                var kvUri = $"https://{Environment.GetEnvironmentVariable("KEY_VAULT_NAME")}.vault.azure.net";
+                var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+                KeyVaultSecret secret = client.GetSecret(name);
+                return secret.Value;
+            }
         }
     }
 }
